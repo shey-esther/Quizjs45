b1c9b1405a97f8e531e027508c904a6f44fa7d23
shey-esther <estheralvaro44@gmail.com>
2017-07-03 13:16:16 -0700

    modifique

--- ejercicio-1.js
+++ ejercicio-1.js
@@ -1,6 +1,6 @@
-var n1 = 10;
-var n2 = 30;
-var n3 = 5;
+var n1 = prompt("Ingrese 1° numero");
+var n2 = prompt("Ingresa 2° numero");
+var n3 = prompt("Ingrese 3° numero");
 var maximo = Math.max(n1, n2, n3);
 var minimo = Math.min(n1, n2, n3);
 console.log(maximo);
